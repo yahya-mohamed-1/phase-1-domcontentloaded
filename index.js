1f0c1e08a07dd561c06c5fa7437ee7bf0edc4200
Yahya Mohamed <171671702+yahya-mohamed-1@users.noreply.github.com>
2024-07-03 16:55:52 +0300

    Complete assignment submission

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+  const paragraph = document.querySelector("p");
+  paragraph.textContent = "This is really cool!";
+});
